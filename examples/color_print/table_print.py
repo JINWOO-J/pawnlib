cf2120a8f28e03d6e1abd211f373ed9afe603553
JINWOO-J <jinwoo@iconloop.com>
2023-02-17 15:01:07 +0900

    Improve a horizontal table.

--- examples/color_print/table_print.py
+++ examples/color_print/table_print.py
@@ -11,7 +11,8 @@ from pawnlib.output.color_print import TablePrinter, PrintRichTable
 data = [
     {
         "address":         "1x038bd14d5ce28a4ac713c21e89f0e6ca5f107f08",
-        "value":         399999999999999966445568,
+        "value":         "[red]399999999999999966445568[/red]",
+        "value2":         399999999999999966445561,
     },
     {
         "address":         "2x038bd14d5ce28a4ac713c21e89f0e6ca5f107f08",
@@ -20,6 +21,7 @@ data = [
     {
         "address":         "3x038bd14d5ce28a4ac713c21e89f0e6ca5f107f08",
         "value":         399999999999999966445568,
+        "value2":         399999999999999966445568,
     }
 ]
 fmt = [
@@ -34,3 +36,16 @@ cprint("\nPrint Table", "white")
 print(TablePrinter()(data))
 
 PrintRichTable(title="RichTable", data=data, columns=["address"], with_idx=False)
+
+PrintRichTable(title="RichTable", data=data[0], with_idx=False)
+
+
+from rich import print
+from rich.table import Table
+
+grid = Table.grid(expand=True)
+grid.add_column()
+grid.add_column(justify="right")
+grid.add_row("Raising shields", "[bold magenta]COMPLETED [green]:heavy_check_mark:")
+
+print(grid)
