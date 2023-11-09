8d077bb1325dc756f8492b4a8310e284b24b56c9
JINWOO-J <jinwoo@iconloop.com>
2023-11-09 15:39:13 +0900

    Add center ellipsis and remove unnecessary output

--- examples/input/text_truncate_test.py
+++ examples/input/text_truncate_test.py
@@ -2,11 +2,18 @@
 import common
 from pawnlib.typing.converter import shorten_text
 
-text = "asdf"*10
+text = "abcd"*10
 
 
-for i in range(1, len(text)):
+for i in range(0, len(text)):
     res = shorten_text(text, width=i)
     print(f"width={i}, len={len(res)}, result={res}")
 
 print()
+
+for i in range(0, len(text)):
+    res = shorten_text(text, width=i, shorten_middle=True, placeholder="...")
+    print(f"width={i}, len={len(res)}, result={res}")
+
+print()
+print(shorten_text("Hello World", width=8))
