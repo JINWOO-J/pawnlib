c491127c07b6099a1772ee72ef5f2980c7d37a42
JINWOO-J <jinwoo@parametacorp.com>
2024-08-29 09:39:53 +0900

    Add int_to_loop_hex function and constants

--- pawnlib/typing/constants.py
+++ pawnlib/typing/constants.py
@@ -24,6 +24,18 @@ class BooleanConstants:
 
 class NumericConstants:
     TINT = 10 ** 18
+    ICX_IN_LOOP = 10**18
+    EXA = 10 ** 18
+    PETA = 10 ** 15
+    TERA = 10 ** 12
+    GIGA = 10 ** 9
+    MEGA = 10 ** 6
+    KILO = 10 ** 3
+    MILLI = 10 ** -3
+    MICRO = 10 ** -6
+    NANO = 10 ** -9
+    SQRT_2 = 2 ** 0.5
+    EULER_MASCHERONI = 0.57721  # Euler-Mascheroni
     PI = 3.14159
     E = 2.71828
     GOLDEN_RATIO = (1 + 5 ** 0.5) / 2
@@ -36,6 +48,10 @@ class AddressConstants:
     # GOVERNANCE_ADDRESS = f"cx{'0'*39}1"
     CHAIN_SCORE_ADDRESS = "cx0000000000000000000000000000000000000000"
     GOVERNANCE_ADDRESS = "cx0000000000000000000000000000000000000001"
+    ETH_ADDRESS = 40  # Ethereum address length without '0x' prefix
+    ETH_ADDRESS_WITH_PREFIX = 42  # Ethereum address length with '0x' prefix
+    BTC_ADDRESS_P2PKH = 34  # Bitcoin P2PKH address length
+    BTC_ADDRESS_BECH32 = 42  # Bitcoin Bech32 address length
 
 
 class TimeStampDigits:
@@ -43,6 +59,8 @@ class TimeStampDigits:
     MILLI_SECONDS_DIGITS = 13
     MICRO_SECONDS_DIGITS = 16
     NANOSECONDS_DIGITS = 19
+    PICOSECONDS_DIGITS = 22
+    FEMTOSECONDS_DIGITS = 25
 
 
 class ICONConstants:
@@ -103,6 +121,78 @@ class HAVAHConstants:
     }
 
 
+class CryptographicConstants:
+    SHA256_DIGEST_SIZE = 32
+    SHA3_256_DIGEST_SIZE = 32
+    KECCAK_256_DIGEST_SIZE = 32
+    SECP256K1_PRIVATE_KEY_SIZE = 32
+    SECP256K1_PUBLIC_KEY_SIZE = 64
+    SECP256K1_SIGNATURE_SIZE = 64
+    ED25519_PRIVATE_KEY_SIZE = 32
+    ED25519_PUBLIC_KEY_SIZE = 32
+    ED25519_SIGNATURE_SIZE = 64
+
+
+class NetworkConstants:
+    DEFAULT_HTTP_PORT = 80
+    DEFAULT_HTTPS_PORT = 443
+    DEFAULT_ICON_PORT = 9000
+    DEFAULT_ETHEREUM_PORT = 8545
+    DEFAULT_BITCOIN_PORT = 8333
+    LOCALHOST = "127.0.0.1"
+    LOCALHOST_IPV6 = "::1"
+    IPV4_LOOPBACK = "127.0.0.1"
+    IPV6_LOOPBACK = "::1"
+    IPV4_BROADCAST = "255.255.255.255"
+    IPV6_BROADCAST = "ff02::1"
+    MAC_ADDRESS_BROADCAST = "ff:ff:ff:ff:ff:ff"
+
+class AWSRegionConstants:
+    REGIONS = {
+        "us-east-1": "US East (N. Virginia)",
+        "us-east-2": "US East (Ohio)",
+        "us-west-1": "US West (N. California)",
+        "us-west-2": "US West (Oregon)",
+        "ca-central-1": "Canada (Central)",
+        "eu-west-1": "EU (Ireland)",
+        "eu-west-2": "EU (London)",
+        "eu-west-3": "EU (Paris)",
+        "eu-central-1": "EU (Frankfurt)",
+        "eu-north-1": "EU (Stockholm)",
+        "eu-south-1": "EU (Milan)",
+        "ap-southeast-1": "Asia Pacific (Singapore)",
+        "ap-southeast-2": "Asia Pacific (Sydney)",
+        "ap-northeast-1": "Asia Pacific (Tokyo)",
+        "ap-northeast-2": "Asia Pacific (Seoul)",
+        "ap-northeast-3": "Asia Pacific (Osaka)",
+        "ap-east-1": "Asia Pacific (Hong Kong)",
+        "ap-south-1": "Asia Pacific (Mumbai)",
+        "sa-east-1": "South America (São Paulo)",
+        "me-south-1": "Middle East (Bahrain)",
+        "af-south-1": "Africa (Cape Town)",
+    }
+
+    @staticmethod
+    def get_name(code: str) -> str:
+        """
+        Get the human-readable name of an AWS region based on its code.
+
+        :param code: AWS region code (e.g., "us-east-1").
+        :return: Human-readable region name (e.g., "US East (N. Virginia)").
+        """
+        return AWSRegionConstants.REGIONS.get(code, "Unknown region")
+
+    @staticmethod
+    def get_list() -> list:
+        """
+        Get a list of all AWS region codes.
+
+        :return: List of AWS region codes.
+        """
+        print(AWSRegionConstants.REGIONS.keys())
+        return list(AWSRegionConstants.REGIONS.keys())
+
+
 class AllConstants(
     TimeConstants,
     BooleanConstants,
@@ -110,12 +200,20 @@ class AllConstants(
     AddressConstants,
     TimeStampDigits,
     ICONConstants,
-    HAVAHConstants
+    HAVAHConstants,
+    CryptographicConstants,
+    NetworkConstants,
+    AWSRegionConstants
 ):
     __slots__ = ()
 
     def __setattr__(self, name, value):
         raise TypeError("Constants are read-only")
 
+    def get_aws_region_name(self, code: str = "") -> str:
+        return self.get_name(code)
+
+    def get_aws_region_list(self) -> list:
+        return self.get_list()
 
 const = AllConstants()
