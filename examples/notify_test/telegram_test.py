204cc0531adbe54182a75533da358629a5dc010f
JINWOO-J <jinwoo@parametacorp.com>
2024-09-11 14:46:05 +0900

    Add asynchronous functionality to TelegramBot
    - Implemented asynchronous message sending using asyncio and aiohttp
    - Added async_mode parameter to TelegramBot constructor to enable async mode
    - Refactored send_message method to support both sync and async modes
    - Introduced send_multiple_messages_async method to send multiple messages concurrently
    - updated examples to demonstrate async usage

--- examples/notify_test/telegram_test.py
+++ examples/notify_test/telegram_test.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 import common
 import os
+import asyncio
 from pawnlib.utils.notify import TelegramBot
 
-# bot_token을 명시적으로 제공하지 않으면 환경변수에서 가져옴
-bot = TelegramBot()
+message_list = (
+    "[HTML] Hello, <b>World</b>! This is a Telegram message with <i>HTML</i> formatting.",
+    {"DICT key": "value", "foo": "bar"},
+    "Plain Hello, World! This is a plain text message."
+)
 
+async def send_telegram_special_char():
+    a_bot = TelegramBot(verify_ssl=False, ignore_ssl_warning=True, async_mode=True)
+    escape_chars = r'_*[]()~`>#+-=|{}.!'
+    tasks = []
+    for idx, escape_char in enumerate(escape_chars):
+        print(f"[{idx}] Preparing to send: {escape_char}")
+        tasks.append(a_bot.send_auto_message_async(f"[{idx}] {escape_char}"))  # 비동기 작업 추가
+    await asyncio.gather(*tasks)
+
+
+bot = TelegramBot(verify_ssl=False, ignore_ssl_warning=True, async_mode=False)
 # 기본 Markdown 메시지 전송
 bot.send_message("1. Hello, *World*! This is a Telegram message with _Markdown_ formatting.")
 
@@ -21,7 +36,10 @@ bot.save_chat_id()
 # 나중에 chat_id를 파일에서 로드
 bot.load_chat_id()
 
-bot.send_auto_message("[HTML] Hello, <b>World</b>! This is a Telegram message with <i>HTML</i> formatting.")
-bot.send_auto_message({"DICT key": "value", "foo": "bar"})
-bot.send_auto_message("Plain Hello, World! This is a plain text message.")
+for message in message_list:
+    bot.send_auto_message(f"[SYNC] {message}")
 
+async_bot = TelegramBot(verify_ssl=False, ignore_ssl_warning=True, async_mode=True)
+async_bot.send_multiple_messages(message_list)
+
+# asyncio.run(send_telegram_special_char())
