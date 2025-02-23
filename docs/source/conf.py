fd5389a3990963217fe766905f402c4297ffe48e
JINWOO-J <jinwoo@parametacorp.com>
2025-02-24 08:52:03 +0900

    Enhance ReadTheDocs SEO

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,6 +75,7 @@ exclude_patterns = []
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['_static']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
