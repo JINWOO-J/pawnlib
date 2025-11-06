c650a3034a9fdd11723fdf4c259c0c96b8d803e1
Git Test User <git.test@example.com>
2025-11-06 16:09:16 +0900

    fix(version): bump version to 2.1.37
    - Update version number in __version__.py from 2.1.36 to 2.1.37
    - Modify http.py to improve verbose logging and response handling
    * Add more detailed response logging for verbose modes
    * Enhance print_response_if_verbose function
    - Update file.py with optional exit parameter in check_file_overwrite
    * Add exit_on_deny parameter to allow more flexible file overwrite handling
    - Improve logging and output configuration in CLI module

--- pawnlib/__version__.py
+++ pawnlib/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'pawnlib'
 __description__ = 'pawnlib is a collection of libraries for IaC.'
 __url__ = 'https://github.com/jinwoo-j/pawnlib'
-__version__ = '2.1.36'
+__version__ = '2.1.37'
 __author__ = 'Jinwoo Jeong'
 __author_email__ = 'jinwoo@parametacorp.com'
 __license__ = 'MIT'
